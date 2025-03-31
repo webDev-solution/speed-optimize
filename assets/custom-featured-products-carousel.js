df94b97ee7c16788f0c9de1a18c87e1c2632717f
bStar928 <brightstar928@hotmail.com>
2025-03-31 07:13:17 -0400

    2nd commit

--- assets/custom-featured-products-carousel.js
+++ assets/custom-featured-products-carousel.js
@@ -10,26 +10,27 @@ class ProductCarouselComponent extends HTMLElement {
     const $carousel = $(this.querySelector(".carousel"));
     if ($carousel.length) {
       $carousel.slick({
-      infinite: true,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      centerMode: true,
-      centerPadding: '0px',
-      arrows:true,
-      prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
-      nextArrow:'<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerPadding: '80px',
-            infinite: true
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0px',
+        arrows:true,
+        prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+        nextArrow:'<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerPadding: '80px',
+              infinite: true
+            }
           }
-        }
-      ]
-    });
+        ]
+      });
+    }
   }
 }
 
